26a4ee02cb8f2906d9d5f749470dd9128563f4ca
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-01-16 21:31:04 -0600

    agregue archivo info para separar el html, y en index agregue boton menu e info

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import data from './data/dataset.js';
 
 console.log(example, renderItems(data), data);
 
-
+/*
 const buttonClear = document.querySelector("button[data-testid='button-clear']");
 const buttonClearName = document.querySelector("button[data-testid='button-clearName']");
 
@@ -13,11 +13,11 @@ const buttonClearName = document.querySelector("button[data-testid='button-clear
 //inputSearch.addEventListener("input", deleteText)
 buttonClear.addEventListener("click", deleteText);
 buttonClearName.addEventListener("click", deleteName)
-/*nada mas son ejemplos para llamar los dos botones*/
+/*nada mas son ejemplos para llamar los dos botones/
 function deleteText() {
   console.log("botón")
 }
 function deleteName() {
   console.log("name");
     
-}
+}*/
--- src/view.js
+++ src/view.js
@@ -1,5 +1,16 @@
 export const renderItems = (data) => {
-  console.log(data)
+  //console.log(data[0].id)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+  const navRoot = document.getElementById("root");
+  const listUl = document.createElement("ul");
+  //const image = document.createElement("img")
+  data.forEach(element => {
+    const listLi = document.createElement("li");
+    listLi.textContent =  `Nombre: ${element.name}
+     imagen: ${element.imageUrl}`;
+    listUl.appendChild(listLi);
+
+  });
+  console.log(navRoot.appendChild(listUl));
+  return navRoot.appendChild(listUl);
 };
