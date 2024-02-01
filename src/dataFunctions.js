780403fafd20f30c562677a3b354e7a81ad30bbb
Gladis Danilu Farill Casiano <danilu080893@hotmail.com>
2024-01-31 21:38:01 -0600

    modificación dataFunctionSpec

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -41,5 +41,5 @@ export const computeStats = (data) => {
   const totalMen = parseFloat((acumuladorMen/data.length)*100).toFixed(2); 
   const totalWomen = parseFloat((acumuladorWomen/data.length)*100).toFixed(2); 
   console.log(typeof parseFloat(totalMen), totalWomen);
-  return `Porcentaje personajes Masculinos \n ${parseFloat(totalMen)}% porcentaje personajes femeninos ${parseFloat(totalWomen)}%`
+  return `Porcentaje personajes masculinos ${totalMen}% Porcentaje personajes femeninos ${totalWomen}%`;
 };
